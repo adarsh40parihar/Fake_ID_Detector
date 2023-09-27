612668ba1ed72213cdf07b88f3b2b652b081f86d
HYP3R_X <146062554+adarsh40parihar@users.noreply.github.com>
2023-09-28 00:33:00 +0530

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import instaloader
 import json
 import csv
-#import os
+# import os
 import sys
 import shutil
 import numpy as np
@@ -12,18 +12,41 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import RandomizedSearchCV, train_test_split
 from scipy.stats import randint
+from instaloader.exceptions import LoginRequiredException
 
-
-pickle_in = open("classifier.pkl","rb")
+pickle_in = open("classifier.pkl", "rb")
 model = pickle.load(pickle_in)
 
-classes = ["",0,1]
+classes = ["", 0, 1]
+
+def myuser1(user):
+    L = instaloader.Instaloader()
+    username = 'sih_2023_14'
+    password = 'qwerty123456'
+
+    try:
+
+        profile = instaloader.Profile.from_username(L.context, user)
+
+    except LoginRequiredException:
+        try:
+            L.load_session_from_file(name)
+            profile = instaloader.Profile.from_username(L.context, user)
+
+        except instaloader.exceptions.QueryReturnedBadRequestException as e:
+            pass
+
+        L.save_session_to_file()
+
+    except Exception as e:
+
+        pass
 
 def scrap(user):
     usrname = user
-    ig=instaloader.Instaloader()
-    
-    profile=instaloader.Profile.from_username(ig.context, usrname)
+    ig = instaloader.Instaloader()
+
+    profile = instaloader.Profile.from_username(ig.context, usrname)
 
     with open(r"./given_test.txt", 'w') as f:
         sys.stdout = f
@@ -32,7 +55,7 @@ def scrap(user):
     sys.stdout = sys.stdout
 
     input_file_name = r"./given_test.txt"
-    str_check= ""
+    str_check = ""
     with open(input_file_name, "r") as input_file:
         line = input_file.readline()
         str_check = input_file.read()
@@ -62,10 +85,10 @@ def scrap(user):
         "FollowerCount": int(profile.followers),
         "FollowingCount": int(profile.followees),
         "BioLength": len(profile.biography),
-        "Posts":  profile.mediacount,
+        "Posts": profile.mediacount,
         "HasProfilePic": profile_pic,
         "IsPrivate": x,
-        "usernameDigitCount": digit_count/len(profile.username)
+        "usernameDigitCount": digit_count / len(profile.username)
     }]
 
     file_path = r"./given_user_json.json"
@@ -86,38 +109,41 @@ def scrap(user):
             csv_writer.writerow(header)
             count += 1
         csv_writer.writerow(data.values())
-    
+
     return data
-       
+
 
 def prediction(ans):
-     prediction = model.predict(ans)
-     return prediction
-     
+    prediction = model.predict(ans)
+    return prediction
+
 
 def main():
-        st.title("Fake Instagram Detector")
-    
-        user = st.text_input("Enter the username")
-        
-        if st.button("Detect"):
-                data = scrap(user)
+    st.title("Fake Instagram Detector")
 
-                ans = []
-                for keys in data:
-                    ans.append(data[keys])
+    user = st.text_input("Enter the username")
+    myuser1(user)
 
-                ans = np.array(ans)
-                ans = ans.reshape(1, -1)
-                fake = prediction(ans)
-                time.sleep(1)
-                
-                st.markdown('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">', unsafe_allow_html=True)
+    if st.button("Detect"):
+        data = scrap(user)
 
-                if fake == 0:     
-                    # st.success('bhai tera to genuine hai')
-                    # st.markdown('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">', unsafe_allow_html=True)
-                    htmlstr = """
+        ans = []
+        for keys in data:
+            ans.append(data[keys])
+
+        ans = np.array(ans)
+        ans = ans.reshape(1, -1)
+        fake = prediction(ans)
+        time.sleep(1)
+
+        st.markdown(
+            '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">',
+            unsafe_allow_html=True)
+
+        if fake == 0:
+            # st.success('bhai tera to genuine hai')
+            # st.markdown('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">', unsafe_allow_html=True)
+            htmlstr = """
                     <style>
                     .green-tick {
                         color: #2ecc71; 
@@ -130,10 +156,10 @@ def main():
                     }
                     </style>
                     """
-                    st.markdown(htmlstr, unsafe_allow_html=True)
-                    st.markdown('<i class="green-tick fas fa-check-circle"></i> Bhai tera genuine hai', unsafe_allow_html=True)
-                else:
-                    htmlstr = """
+            st.markdown(htmlstr, unsafe_allow_html=True)
+            st.markdown('<i class="green-tick fas fa-check-circle"></i> Bhai tera genuine hai', unsafe_allow_html=True)
+        else:
+            htmlstr = """
                     <style>
                     .icon {
                         color: #ff0000; 
@@ -146,10 +172,9 @@ def main():
                     }
                     </style>
                     """
-                    st.markdown(htmlstr, unsafe_allow_html=True)
-                    st.markdown('<i class="icon fas fa-times"></i> Tu to gyo', unsafe_allow_html=True)
-        
-        
-if __name__=='__main__':
+            st.markdown(htmlstr, unsafe_allow_html=True)
+            st.markdown('<i class="icon fas fa-times"></i> Tu to gyo', unsafe_allow_html=True)
+
+
+if __name__ == '__main__':
     main()
-        
